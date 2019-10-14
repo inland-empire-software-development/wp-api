a9982addcbb5301205af058e2ced4d325e2d9a9d
lloan <lloanalas@outlook.com>
2019-10-14 07:44:28 -0700

    added new post types

--- inc/graphql.php
+++ inc/graphql.php
@@ -7,18 +7,22 @@ add_filter( 'register_post_type_args', function( $args, $post_type ) {
 		$args['graphql_single_name'] = 'Staff';
 		$args['graphql_plural_name'] = 'Staff';
 	}
-
-	if ( 'speakers' === $post_type ) {
+	elseif ( 'speakers' === $post_type ) {
 		$args['show_in_graphql'] = true;
 		$args['graphql_single_name'] = 'Speaker';
 		$args['graphql_plural_name'] = 'Speakers';
-	}
-
-
-	if ( 'community' === $post_type ) {
+	} elseif ( 'community' === $post_type ) {
 		$args['show_in_graphql'] = true;
 		$args['graphql_single_name'] = 'Community';
 		$args['graphql_plural_name'] = 'Community';
+	} elseif ( 'story' === $post_type ) {
+		$args['show_in_graphql'] = true;
+		$args['graphql_single_name'] = 'Story';
+		$args['graphql_plural_name'] = 'Stories';
+	} elseif ( 'sponsor' === $post_type ) {
+		$args['show_in_graphql'] = true;
+		$args['graphql_single_name'] = 'Sponsor';
+		$args['graphql_plural_name'] = 'Sponsors';
 	}
 
 	return $args;
