bc7f1309117bd8897c55071282f6b028c342ac2b
lloan <lloanalas@outlook.com>
2019-10-28 23:51:26 -0700

    expose settings to graphql

--- inc/graphql.php
+++ inc/graphql.php
@@ -1,28 +1,42 @@
 <?php
 
-add_filter( 'register_post_type_args', function( $args, $post_type ) {
+add_filter( 'register_post_type_args', function ( $args, $post_type ) {
+	$plural   = false;
+	$singular = false;
 
-	if ( 'staff' === $post_type ) {
-		$args['show_in_graphql'] = true;
-		$args['graphql_single_name'] = 'Staff';
-		$args['graphql_plural_name'] = 'Staff';
+	switch ( $post_type ) {
+		case "staff":
+			$plural   = "Staff";
+			$singular = "Staff";
+			break;
+		case "speakers":
+			$plural   = "Speakers";
+			$singular = "Speaker";
+			break;
+		case "community":
+			$plural   = "Community";
+			$singular = "Community";
+			break;
+		case "story":
+			$plural   = "Stories";
+			$singular = "Story";
+			break;
+		case "sponsor":
+			$plural   = "Sponsors";
+			$singular = "Sponsor";
+			break;
+		case "settings":
+			$plural   = "Settings";
+			$singular = "Setting";
+			break;
+		default:
+			break;
 	}
-	elseif ( 'speakers' === $post_type ) {
-		$args['show_in_graphql'] = true;
-		$args['graphql_single_name'] = 'Speaker';
-		$args['graphql_plural_name'] = 'Speakers';
-	} elseif ( 'community' === $post_type ) {
-		$args['show_in_graphql'] = true;
-		$args['graphql_single_name'] = 'Community';
-		$args['graphql_plural_name'] = 'Community';
-	} elseif ( 'story' === $post_type ) {
-		$args['show_in_graphql'] = true;
-		$args['graphql_single_name'] = 'Story';
-		$args['graphql_plural_name'] = 'Stories';
-	} elseif ( 'sponsor' === $post_type ) {
-		$args['show_in_graphql'] = true;
-		$args['graphql_single_name'] = 'Sponsor';
-		$args['graphql_plural_name'] = 'Sponsors';
+
+	if ( $plural && $singular ) {
+		$args['show_in_graphql']     = true;
+		$args['graphql_single_name'] = $singular;
+		$args['graphql_plural_name'] = $plural;
 	}
 
 	return $args;
